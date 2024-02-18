0f5a33cdfe96c4da4b3383d2e8df80ab606dfe95
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2024-02-18 18:34:19 -0500

    Update files from linus

--- Video/server.js
+++ Video/server.js
@@ -1,47 +1,52 @@
-let express = require('express');
-let app = express();
+var https = require('https');
+var fs = require('fs'); // Using the filesystem module
+
+var credentials = { 
+key: fs.readFileSync('/etc/letsencrypt/live/qz2432.itp.io/privkey.pem'), 
+cert: fs.readFileSync('/etc/letsencrypt/live/qz2432.itp.io/cert.pem') 
+};
+
+// Express Code
+var express = require('express');
+var app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/', function(req, res) {
+	res.send("Hello World!");
 });
 
-var http = require('http');
-var httpServer = http.createServer(app);
-httpServer.listen(80);
+var httpsServer = https.createServer(credentials, app);
+// Default HTTPS Port 443
+httpsServer.listen(443);
 
 // WebSocket Portion
-// WebSockets work with the HTTP server
-var io = require('socket.io')(httpServer);
+// WebSockets work with the HTTPS server
+var io = require('socket.io')(httpsServer);
+
+const userIds = [
+	"AppleHarmony", "BerryWhisperer", "CherryDreamer", "GrapeExplorer", "KiwiJourney",
+    "MangoMelody", "OrangeSunset", "PineappleBreeze", "StrawberrySerene", "WatermelonWanderer",
+    "BananaBliss", "BlueberryBlaze", "CoconutCraze", "DragonfruitDancer", "FigFantasy",
+    "GuavaGlimmer", "LemonLullaby", "LimeLagoon", "PassionfruitPrelude", "PeachPonder",
+    "PearPebble", "PlumPilgrim", "RaspberryRipple", "StarfruitSerenade", "TangerineTranquil",
+    "UmbuCalm", "VanillaVista", "ZestZenith"
+	]
+const user = []
+let ptr = 0
 
 io.sockets.on('connection', 
 	// We are given a websocket object in our function
 	function (socket) {
 		console.log("We have a new client: " + socket.id);
-		
-		//draw socket
-		socket.on('draw', function(data) {
-			console.log("draw: " + data)
-			io.emit('draw', data)
-		});
+		const id = userIds[ptr]
+		console.log("The new client: " + id)
+		ptr += 1
 
-		//prompt socket
-		socket.on('prompt', function(data) {
-			console.log("prompt: " + data)
-			io.emit('prompt', data)
-		})
+		io.emit('connection', id)
 
-		//clear canvas socket
-		socket.on('clearCanvas', function(){
-			console.log("clearCanvas")
-			io.emit('clearCanvas')
-		})
-
-		//score socket
-		socket.on('score', function(data){
-			console.log("score", data)
-			io.emit('score', data)
+		socket.on('video', function(data){
+			socket.broadcast.emit('video', data)
 		})
 		
 		socket.on('disconnect', function() {
