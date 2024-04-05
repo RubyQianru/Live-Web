1178b5a39e5063e94b45fb00955d26d93fac61d6
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2024-04-05 15:10:53 -0400

    presentation file update

--- Presentation/server.js
+++ Presentation/server.js
@@ -1,40 +1,106 @@
-var https = require('https');
-var fs = require('fs'); // Using the filesystem module
-
-var credentials = { 
-key: fs.readFileSync('/etc/letsencrypt/live/qz2432.itp.io/privkey.pem'), 
-cert: fs.readFileSync('/etc/letsencrypt/live/qz2432.itp.io/cert.pem') 
-};
-
-// Express Code
+// We need the file system here
+var fs = require('fs');
+				
+// Express is a node module for building HTTP servers
 var express = require('express');
 var app = express();
 
+// Tell Express to look in the "public" folder for any files first
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-	res.send("Hello World!");
+// If the user just goes to the "route" / then run this function
+app.get('/', function (req, res) {
+  res.send('Hello World!')
 });
 
-var httpsServer = https.createServer(credentials, app);
-// Default HTTPS Port 443
-httpsServer.listen(443);
+// Here is the actual HTTP server 
+// In this case, HTTPS (secure) server
+var https = require('https');
+
+// Security options - key and certificate
+var options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/sc7436.itp.io/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/sc7436.itp.io/cert.pem')
+};
+
+// We pass in the Express object and the options object
+var httpServer = https.createServer(options, app);
+
+// Default HTTPS port
+httpServer.listen(443);
+
+/* 
+This server simply keeps track of the peers all in one big "room"
+and relays signal messages back and forth.
+*/
+
+let count = 0;
 
 // WebSocket Portion
-// WebSockets work with the HTTPS server
-var io = require('socket.io')(httpsServer);
+// WebSockets work with the HTTP server
+// Using Socket.io
+const { Server } = require('socket.io');
+const io = new Server(httpServer, {});
 
+const Datastore = require('nedb');
+const db = new Datastore({ filename: 'data.db', autoload: true });
+
+// Old version of Socket.io
+//var io = require('socket.io').listen(httpServer);
+
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
 io.sockets.on('connection', 
+
 	// We are given a websocket object in our function
 	function (socket) {
 		console.log("We have a new client: " + socket.id);
 
-		io.emit('connection', id)
-	
-		socket.on('disconnect', function() {
-			console.log("Client has disconnected " + socket.id);
-		});
-	}
-);
+        db.find({}, (err, votes) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            socket.emit("loadHistory", votes);
+        });
 
-
+    
+        socket.on('vote', (option) => {
+            db.findOne({ option }, (err, existingEntry) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+        
+                if (existingEntry) {
+                    db.remove({ _id: existingEntry._id }, {}, (err, numRemoved) => {
+                        if (err) {
+                            console.error(err);
+                            return;
+                        }
+                        
+                        db.insert({ name: "Q1", option, count: existingEntry.count + 1 }, (err, newDocs) => {
+                            if (err) {
+                                console.error(err);
+                                return;
+                            }
+                            io.emit('voteUpdate', option);
+                        });
+                    });
+                } else {
+                    db.insert({ name: "Q1", option, count: 1 }, (err, newDocs) => {
+                        if (err) {
+                            console.error(err);
+                            return;
+                        }
+                        io.emit('voteUpdate', option);
+                    });
+                }
+            });
+        });
+        
+    
+        socket.on('disconnect', () => {
+            console.log('A client disconnected');
+        });
+});
